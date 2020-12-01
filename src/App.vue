7cbc57f79d15b043b4e86b9736cb02ef092efa0d
xinshaoguang <997888255@qq.com>
2020-12-01 14:00:22 +0800

    添加刷新保持store数据方法

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,31 @@
     <router-link to="/state">state</router-link> |
     <router-link to="/getters">getter</router-link> |
     <router-link to="/mutations">mutations</router-link> |
-    <router-link to="/actions">actions</router-link> 
+    <router-link to="/actions">actions</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
+<script>
+export default {
+  created() {
+    //在页面加载时读取sessionStorage里的状态信息
+    if (sessionStorage.getItem("store")) {
+      this.$store.replaceState(
+        Object.assign(
+          {},
+          this.$store.state,
+          JSON.parse(sessionStorage.getItem("store"))
+        )
+      );
+    }
+    //在页面刷新时将vuex里的信息保存到sessionStorage里
+    window.addEventListener("beforeunload", () => {
+      console.log(123)
+      sessionStorage.setItem("store", JSON.stringify(this.$store.state));
+    });
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
